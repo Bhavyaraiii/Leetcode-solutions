5bd68867c2bc66c36f286432e4d154610cf1c0a3
Bhavyaraiii <bhavya8007@gmail.com>
2026-08-18 11:47:49 +0530

    Time: 0 ms (100%), Space: 43.6 MB (78.63%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -9,20 +9,25 @@ public:
         int currMin = 0;
         int minSum = nums[0];
 
-        for (int x : nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int x = nums[i];
+
             
             currMax = max(x, currMax + x);
             maxSum = max(maxSum, currMax);
 
-            
+        
             currMin = min(x, currMin + x);
             minSum = min(minSum, currMin);
 
             totalSum += x;
         }
+
+        
         if (maxSum < 0) {
             return maxSum;
         }
+
         return max(maxSum, totalSum - minSum);
     }
 };
